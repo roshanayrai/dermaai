2cdef02e4a768bf927b26f80121e02601971649b
roshanayrai <roshanayrai@gmail.com>
2024-01-07 00:44:01 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { homepage } from './pages/homepage';
 function App() {
     return (
       <div className="App">
@@ -10,7 +9,7 @@ function App() {
             </div>
             <nav className="Header-nav">
               <ul>
-                <li><a href="homepage">Skin Identification</a></li>
+                <li><a href="">Skin Identification</a></li>
                 <li><a href="#ingredient-analysis">Ingredient Analysis</a></li>
                 <li><a href="#dermatologist-recommendation">Dermatologist Recommendation</a></li>
                 <li><a href="#skin-identification">Product Reommendation</a></li>

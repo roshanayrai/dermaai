f59771bfb988f0c9f506fb966b2fb95406563b6a
roshanayrai <roshanayrai@gmail.com>
2024-01-07 21:34:54 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
 function App() {
     return (
       <div className="App">
         <header className="App-header">
             <Navbar></Navbar>
-        
+            <Homepage></Homepage>
         </header>
       </div>
     );
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import "./Homepage.css"
 const Homepage = () =>{
     return(
         <>
-        <h1>Hello</h1>
+        <div className="section1">
+         <div className='maintext'>
+            <h1>Get answers to your skin condition questions with DermaAI</h1>
+            <p>Aysa is the easy-to-use app for concerns about common skin conditions. Powered by VisualDx, Aysa provides personalized guidance and helpful information to aid in your discussion with your healthcare provider.</p>
+         </div>
+         <div className='section1img'>
+         <img src="homepage1.png" />
+         </div>
+        </div>
+        <div className="section2">
+         <div className='maintext'>
+            <h1>Are you a part of our community yet? Join today and use our AI Skin tools without a hitch.</h1>
+         </div>
+         
+         <button className="button">Register Now</button>
+         </div>
+        
         </>
     );
 }

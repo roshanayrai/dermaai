deb3e8cf25100e9c394b1a2bd6f2d6cefdfc9754
MairaShafiq <92164311+MairaShafiq@users.noreply.github.com>
2024-01-07 19:39:33 +0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,30 +3,7 @@ import './Login.css';
 
 function App() {
  return (
-    <div className="App">
-    <div className="navbar">
-     <a href="#" class="right">DermaAI</a>
-     <a href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-        <a href="#">Contact Us</a>
-        <a href="#">News/Blogs</a>
-        <div class="dropdown">
-  <button class="dropbtn">tools
-  <i class="fa fa-caret-down"></i>
-  </button>
-  <div class="dropdown-content" id="myDropdown">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-  </div> 
-        <a href="#">About Us</a>
-      </div>
-      
+    <div className="App">  
       <h1>Sign In to Your Account</h1>
       <h3>Enter required details to proceed</h3>
       <div className="login">
@@ -49,4 +26,4 @@ function App() {
  );
 }
 
-export default App;
+export default Login;

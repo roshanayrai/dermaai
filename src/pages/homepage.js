9476001684355dd218e0b80385d1a85011c97ea3
roshanayrai <roshanayrai@gmail.com>
2024-01-09 20:57:26 +0500

    commit

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -7,14 +7,7 @@ const Homepage = () =>{
     return(
         <>
         <div className='Homepage'><Navbar></Navbar></div>
-        <div className="section1">
-         <div className='section1text'>
-            <h1>Get answers to your skin condition questions with DermaAI</h1>
-            <p>Aysa is the easy-to-use app for concerns about common skin conditions. Powered by VisualDx, Aysa provides personalized guidance and helpful information to aid in your discussion with your healthcare provider.</p>
-         </div>
-         <div className='section1img'><img src="homepage1.png" />
-         </div>
-        </div>
+    
         <div className="section2">
          <div className='section2text'>
             <h1>Are you a part of our community yet? Join today and use our AI Skin tools without a hitch.</h1>
